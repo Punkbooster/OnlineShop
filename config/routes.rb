499d160467c70c86ccf813dae9f8baae8f2c473e
Punkbooster <2morrow2late@rambler.ru>
2015-11-03 01:41:11 +0100

    Generate devise routes & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+ 
   resources :products
+  
   root "products#index"
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 end
