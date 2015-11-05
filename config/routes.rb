92bc37abd9560ca1cbd8dd097afb8219298a6496
Punkbooster <2morrow2late@rambler.ru>
2015-11-05 18:15:02 +0100

    Generate contollers & update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
- 
-  resources :products
-  
-  root "products#index"
+
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+  root to: "products#index"
 
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 end
